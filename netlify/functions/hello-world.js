0299b3da2d54350afc5bcef43f8bb72315d20e77
Boddupally <venkatsairaj.boddupally@abbvie.com>
2023-06-15 16:37:14 -0500

    nnetlify serverless

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,10 +1,18 @@
 import fetch from 'node-fetch'
 
 export const handler = async () => {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+  //const POKE_API = 'https://api.abbvie.com/patientrm/hcp-locator/physicianlocator'
 
-  const response = await fetch(POKE_API)
+  //const response = await fetch(POKE_API)
+  //const data = await response.json()
+
+  const response = await fetch("https://api-pq.abbvie.com/patientonboarding/getdetails/supportgroups?zip=60090&miles=50", {
+    method: "get",
+    headers: { "Content-Type": "application/json" , "Authorization" : "Basic NzI0NTQ0NWFiMDk0Y2E0YThiMmQ2NzkyOThjMTZjNmU6NjgwYTIyYjExMGQ2MWU0YTkzYTI5YjFhYzY2MzhiYjY="}
+  });
+
   const data = await response.json()
+  console.log(data)
 
   return {
     statusCode: 200,
