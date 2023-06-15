51aa514cf3a029dfab43ffa0738bd2ef9da09c2e
Boddupally <venkatsairaj.boddupally@abbvie.com>
2023-06-15 17:33:50 -0500

    nnetlify serverless post

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -5,10 +5,35 @@ export const handler = async () => {
 
   //const response = await fetch(POKE_API)
   //const data = await response.json()
+  const body = {
+                   "PhysicianLocatorRequest": {
+                       "PhysicianLocatorRequestHeader": {
+                           "PublisherRequestId": "1234102",
+                           "PublisherName": "RINVOQ"
+                       },
+                       "PhysicianLocatorRequestBody": {
+                           "PhysicianAddress": {
+                               "Zip": "60004"
+                           },
+                           "SpecialtyName": "Immunology - Rheum",
+                           "IndicationId": 10020,
+                           "SearchRadius": "25",
+                           "TermsConditionsCheck": "true",
+                           "RecordsFrom": "1",
+                           "RecordCount": "10",
+                           "SearchUnBranded":"No",
+                           "GeoCoordinates" : {
+                               "Latitude" : "",
+                               "Longitude" : ""
+                           }
+                       }
+                   }
+               };
 
-  const response = await fetch("https://api-pq.abbvie.com/patientonboarding/getdetails/supportgroups?zip=60090&miles=50", {
-    method: "get",
-    headers: { "Content-Type": "application/json" , "Authorization" : "Basic NzI0NTQ0NWFiMDk0Y2E0YThiMmQ2NzkyOThjMTZjNmU6NjgwYTIyYjExMGQ2MWU0YTkzYTI5YjFhYzY2MzhiYjY="}
+  const response = await fetch("https://api.abbvie.com/patientrm/hcp-locator/physicianlocator", {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" , "Authorization": "Basic NTBhNjE0OTEzODdiMTI0MDg0N2Y5MDUzMmJhYzNjZDA6YzJjYmFjMjI0Y2U1NmE0MmExMzEzM2E2OGRlM2Y3MzE"}
   });
 
   const data = await response.json()
